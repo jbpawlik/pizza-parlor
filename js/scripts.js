52104ef0f116bc620c9efeb31e7bb9f6e37d80f6
Ben Pawlik <james.benjamin.pawlik@gmail.com>
2021-06-18 08:59:34 -0700

    add Order constructor

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,30 @@
 // Business Logic
 
-function CustomPizza(toppings, size, price) {
-  this.toppings = toppings
+//Make new order
+function Order(pizzas, totalPrice) {
+  this.pizzas = [pizzas]
+  this.totalPrice = totalPrice
+}
+
+
+//Make custom pizza
+function CustomPizza(name, toppings, size, price) {
+  this.name = name
+  this.toppings = [toppings]
   this.size = size
   this.price = price
 }
 
-let pizza1 = new CustomPizza('none', 'regular', '$10')
 
-pizza1
+//Add new pizza to order
 
+Order.prototype.addPizza = function(pizza) {
+  this.pizzas[pizza.name] = pizza
+}
+
+let pizza1 = new CustomPizza('plain', 'none', 'regular', '$10')
+let pizza2 = new CustomPizza('large extra cheese',' extra cheese', 'large', '$15')
+let order1 = new Order([pizza1, pizza2], '$25')
+order1.addPizza([pizza1, pizza2])
 
 //User Interface Logic
