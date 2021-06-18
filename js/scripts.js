5a1db92225400747c682d9fc5127194bb2294af1
Ben Pawlik <james.benjamin.pawlik@gmail.com>
2021-06-18 09:14:00 -0700

    name pizza based off size and topping

--- js/scripts.js
+++ js/scripts.js
@@ -8,24 +8,27 @@ function Order(pizzas, totalPrice) {
 
 
 //Make custom pizza
-function CustomPizza(name, toppings, size, price) {
+function CustomPizza(name, topping, size, price) {
   this.name = name
-  this.toppings = [toppings]
+  this.topping = topping
   this.size = size
   this.price = price
 }
-
 
 //Add new pizza to order
 Order.prototype.addPizza = function(pizza) {
   this.pizzas[pizza.name] = pizza
 }
 
-//
+//Name the pizza based off size and toppings
+CustomPizza.prototype.namePizza = function() {
+  this.name = this.size + " " + this.topping
+}
 
 let pizza1 = new CustomPizza('plain', 'none', 'regular', '$10')
 let pizza2 = new CustomPizza('large extra cheese', 'extra cheese', 'large', '$15')
 let order1 = new Order([pizza1, pizza2], '$25')
+pizza1.namePizza(pizza1)
 order1.addPizza([pizza1, pizza2])
 
 //User Interface Logic
