765fe2c0ff5f86eea645e266bb0650f3cba800ce
Ben Pawlik <james.benjamin.pawlik@gmail.com>
2021-06-19 14:29:49 -0700

    allow multiple pizzas to be added

--- js/scripts.js
+++ js/scripts.js
@@ -44,58 +44,91 @@ if (this.size === 'XL') {
   return this.price
 }
 
+// this.name = ''
+// this.name = this.size + " " + this.toppings
 }
 
+
+// if (this.topping === 'extra cheese') {
+//     this.price = this.price + 2;
+//   }
+//   if (this.topping.length > 0) {
+//     this.price = this.price + 2;
+//   }
+
+
+// //Name the pizza based off size and toppings
+// CustomPizza.prototype.namePizza = function() {
+//   this.name = this.size + " " + this.topping
+// }
+
+// let pizza1 = new CustomPizza(['Cheese', 'Meat'], 'Regular', 10)
+// let pizza2 = new CustomPizza(['Extra Cheese'], 'XL', 15)
+// let order1 = new Order([], 0, 0)
+// // pizza1.namePizza(pizza1)
+// pizza1.makePizza()
+// pizza2.makePizza()
+// order1.addPizza(pizza1)
+// order1.addPizza(pizza2)
 
 //User Interface Logic
 $(document).ready(function() {
   attachContactListeners()
 
   let order = new Order()
-
-  const size = $('input:radio:checked[name=size]').val()
-
-  let toppingArray = $('.topping:checked').map(function() {
-    return this.value;
-  }) .get();
-
-  let name = size + " " + toppingArray.join(", ")    
   
-  let pizza = new CustomPizza(name, toppingArray, size)
-
+  let pendingOrders = 0
 
   $('button#addToOrder').click(function(event) {
     event.preventDefault();
 
     $('#pendingOrder').show();
-    $('.col-3').show();
+
+    const size = $('input:radio:checked[name=size]').val()
     
-    let pizza1 = pizza
+    let toppingArray = $('.topping:checked').map(function() {
+      return this.value;
+    }) .get();
+
+    let name = size + " " + toppingArray.join(", ")    
     
-    if (pizza.number === 0) {
-    pizza.makePizza() 
-    console.log(pizza1);
-    } else if (pizza.number > 0) {
-      
-    }
-
-
-    $('#prepLine').html('<br>' + pizza1.name + '<br><br> Price: ' + pizza1.price)
-
-    // let order = new Order()
+    if (pendingOrders === 0) {
+    let pizza1 = new CustomPizza(name, toppingArray, size)
+    pizza1.makePizza() 
+    $('#prepLine1').html(pizza1.number + '<br>' + pizza1.name + '<br><br> Price: ' + pizza1.price)
+    $('#col1').show()
     order.addPizza(pizza1)
-    console.log(order)
+    pendingOrders = 1;
+    } else if (pendingOrders === 1) {
+    let pizza2 = new CustomPizza(name, toppingArray, size)
+    pizza2.makePizza() 
+    $('#col2').show()
+    $('#prepLine2').html(pizza2.number + '<br>' + pizza2.name + '<br><br> Price: ' + pizza2.price)
+    order.addPizza(pizza2);
+    pendingOrders = 2;
+    } else if (pendingOrders === 2) {
+    let pizza3 = new CustomPizza(name, toppingArray, size)
+    pizza3.makePizza() 
+    $('#col3').show()
+    $('#prepLine3').html(pizza3.number + '<br>' + pizza3.name + '<br><br> Price: ' + pizza3.price)
+    order.addPizza(pizza3)
+    pendingOrders = 3;
+    
+  }
+    $('#totalPrice').text(order.totalPrice)
+  console.log(order)
+    // let order = new Order()
+    // order.addPizza(pizza)
+    // console.log(order)
   });
 
 
   function attachContactListeners() {
-    $('button#removePizza').click(function(event) {
+    $('button.removePizza').click(function(event) {
       event.preventDefault();
-      $('.col-3').hide();
-      delete pizza.number;
-      order.totalPrice = 0
-      console.log(pizza)
-      console.log(order)
+
+
+
     });
   }
 
